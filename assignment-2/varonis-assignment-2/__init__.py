43bba6481be879ccc91f4518ca5ce384fc131c87
unknown <m1992w@gmail.com>
2021-11-15 11:17:31 -0500

    Added additional comments for assignment-2

--- assignment-2/varonis-assignment-2/__init__.py
+++ assignment-2/varonis-assignment-2/__init__.py
@@ -3,12 +3,15 @@ import logging
 import azure.functions as func
 from azure.keyvault import secrets
 
-
+#main function to be executed when a HTTP request is initialized
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    #defining variable to be used for supplied parameter
     global name
     name = req.params.get('name')
+
+    #if name not found, execute these 
     if not name:
         try:
             req_body = req.get_json()
@@ -17,6 +20,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
+    #if name found, execute the check_secret function; displays some text on the web page that will provide us the values we need
     if name:
         check_secret()
         return func.HttpResponse(f"Hello! This HTTP triggered function executed successfully. \n Key Vault name: {name} \n Key Vault secret name: {secret.name} \n Secret creation date: {secret.properties.created_on} \n Key vault secret value: {secret.value}")
@@ -26,15 +30,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
     
-
+# this function this will search Azure Key Vault for the requested name
 def check_secret():
     from azure.identity import DefaultAzureCredential
     from azure.keyvault.secrets import SecretClient
 
+    #sets our credentials set
     credential = DefaultAzureCredential()
+    #instantiates a client session to retrieve our secrets
     client = SecretClient(
         vault_url=f"https://{name}.vault.azure.net",
         credential=credential
     )
+    #sets the variable to global, so it can be used by main()
     global secret
     secret = client.get_secret("VaronisAssignmentSecret")
