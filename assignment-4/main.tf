ca6586e7f631fa8f6fa6444058e2a7967763d3a0
unknown <m1992w@gmail.com>
2021-11-16 22:55:23 -0500

    Added northeurope modules

--- assignment-4/main.tf
+++ assignment-4/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   tenant_id = "a00bd783-ae68-4960-b694-bf4b59a40f10"
 }
 
-module "resource_group" {
+module "resource_group_eastus" {
   source = "./modules/resource_group"
 
   resource_group_name = "varonis-assignment-4"
@@ -21,19 +21,42 @@ module "resource_group" {
   
 }
 
-
-module "network" {
+module "network_eastus" {
   source = "./modules/network"
 
-  az_rg_name = module.resource_group.az_rg.name
-  az_rg_location = module.resource_group.az_rg.location
+  az_rg_name = module.resource_group_eastus.az_rg.name
+  az_rg_location = module.resource_group_eastus.az_rg.location
 }
 
-module "vm" {
+module "vm_eastus" {
   source = "./modules/vm"
 
-  az_ni = module.network.az_ni
-  az_rg_name = module.resource_group.az_rg.name
-  az_rg_location = module.resource_group.az_rg.location
+  az_ni = module.network_eastus.az_ni
+  az_rg_name = module.resource_group_eastus.az_rg.name
+  az_rg_location = module.resource_group_eastus.az_rg.location
+
+}
+
+module "resource_group_northeurope" {
+  source = "./modules/resource_group"
+
+  resource_group_name = "varonis-assignment-4"
+  resource_group_location = "northeurope"
+  
+}
+
+module "network_northeurope" {
+  source = "./modules/network"
+
+  az_rg_name = module.resource_group_northeurope.az_rg.name
+  az_rg_location = module.resource_group_northeurope.az_rg.location
+}
+
+module "vm_northeurope" {
+  source = "./modules/vm"
+
+  az_ni = module.network_northeurope.az_ni
+  az_rg_name = module.resource_group_northeurope.az_rg.name
+  az_rg_location = module.resource_group_northeurope.az_rg.location
 
 }
